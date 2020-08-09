735cb03dde95d1dc909ca2cd4a1fcdc83a0e9273
JIm <kstsabc@gmail.com>
2020-08-09 15:28:48 +0800

    初始化專案-非DI情境

---
+++ WazeCredit/Service/MarketForecaster.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WazeCredit.Models;
+
+namespace WazeCredit.Service
+{
+    public class MarketForecaster
+    {
+        public MarketResult GetMarketPrediction()
+        {
+            return new MarketResult
+            {
+                MarketCondition = MarketCondition.StableUp
+            };
+        }
+    }
+
+    public class MarketResult
+    {
+        public MarketCondition MarketCondition { get; set; }
+    }
+}
