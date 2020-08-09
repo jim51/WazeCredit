aa9b5d43f86528e4ea879f657083f238647d5e7e
JIm <kstsabc@gmail.com>
2020-08-09 21:20:23 +0800

    IValidateChecker及地址薪水檢查

---
+++ WazeCredit/Service/CreditValidationChecker.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WazeCredit.Models;
+
+namespace WazeCredit.Service
+{
+    public class CreditValidationChecker : IValidateionChecker
+    {
+        public string ErrorMessage => "不符合 年齡/薪水 條件";
+
+        public bool ValidatorLogic(CreditApplication model)
+        {
+            if(model.PostalCode<=0 || model.PostalCode >= 99999)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
