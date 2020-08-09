d2e5266d5a990b1377e7504ae25165af5a9a0e1f
JIm <kstsabc@gmail.com>
2020-08-09 18:14:26 +0800

    建立三個DI生命週期類別

---
+++ WazeCredit/Service/LifeTimeExample/SingletionService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WazeCredit.Service.LifeTimeExample
+{
+    public class SingletionService
+    {
+        private readonly Guid guid;
+
+        public SingletionService()
+        {
+            guid = Guid.NewGuid();
+        }
+
+        public string GetGuid() => guid.ToString();
+    }
+}
---
+++ WazeCredit/Service/LifeTimeExample/TransientService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WazeCredit.Service.LifeTimeExample
+{
+    public class TransientService
+    {
+        private readonly Guid guid;
+
+        public TransientService()
+        {
+            guid = Guid.NewGuid();
+        }
+
+        public string GetGuid() => guid.ToString();
+    }
+}
